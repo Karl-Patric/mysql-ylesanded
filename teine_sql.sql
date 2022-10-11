e97a730b1fc920d47a518aa65944f327ab5bbd89
Karl-Patric <92147164+Karl-Patric@users.noreply.github.com>
2022-10-11 14:39:22 +0300

    Update teine_sql.sql

--- teine_sql.sql
+++ teine_sql.sql
@@ -1,13 +1,7 @@
-SELECT * FROM d110079sd465665.orders;
-SELECT sum(stock_saldo*price) FROM books;
-SELECT MIN(price) AS Kõige_odavam, MAX(price) AS Kõige_kallim, AVG(price) AS Keskmine FROM books;
-SELECT MAX(price) AS Kõige_kallim, from books WHERE type 'used';
-SELECT CASE WHEN type = "new" THEN "uus" WHEN type = "used" THEN "kasutatud" WHEN type = "ebook" THEN "e-raamat" END as tüüp, round(avg(price),2) as Keskmine_Hind, COUNT(*) As Hulk FROM books GROUP BY type;
-SELECT * FROM books WHERE type = "used" AND price > (SELECT avg(price) FROM books WHERE type = 'new');
-SELECT * FROM books WHERE price > (SELECT avg(price) FROM authors
-LEFT JOIN book_authors ON books.id = book_authors.author_id = authors_id
-LEFT JOIN books ON books_id = book_authors.book_id 
-LEFT JOIN orders ON orders.book_id = book.id 
-GROUP BY authors.id ORDER BY count(*) desc LIMIT 1);
-SELECT * FROM books WHERE release_date % 2 = 0;
-SELECT count(*), language FROM books GROUP BY language ORDER BY count(*) desc LIMIT 3;
+SELECT * FROM d110072_raamatud.books WHERE price > ALL(SELECT avg(price) FROM books);
+SELECT sum(stock_saldo*price) FROM d110072_raamatud.books;
+SELECT  min(price) as koige_odavam, max(price) as koige_kallim, avg(price) as keskmine_hind from d110072_raamatud.books;
+SELECT max(price) as koige_kallim FROM d110072_raamatud.books WHERE type = "used";
+SELECT CASE when type = "new" then "uus"  WHEN type = "used" then "kasutatud" WHEN type = "ebook" then "eraamat" END as Tüüp, round(avg(price),2) as Keskmine_hind, COUNT(*) as HULK FROM d110072_raamatud.books GROUP BY type;
+SELECT * FROM d110072_raamatud.books WHERE type = "used" AND price > (SELECT avg(price) FROM d110072_raamatud.books WHERE type = "new" );
+SELECT * FROM d110072_raamatud.books WHERE price > (SELECT avg(price) FROM d110072_raamatud.authors LEFT JOIN book_authors ON book_authors.author_id = authors.id LEFT JOIN books ON books.id  = book_authors.book_id LEFT JOIN orders ON orders.book_id = books.id GROUP BY authors.id ORDER BY count(*) desc LIMIT 1);
