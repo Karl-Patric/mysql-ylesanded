b18b42b6e2caac1929e7d38192b5c0b558d7dd1d
Karl-Patric <92147164+Karl-Patric@users.noreply.github.com>
2022-10-11 14:54:58 +0300

    Update Kolmas mysql.sql

--- Kolmas mysql.sql
+++ Kolmas mysql.sql
@@ -1,19 +1,19 @@
 SELECT * FROM d110072_raamatud.books WHERE release_date % 2 = 0;
 SELECT count(*), language FROM d110072_raamatud.books GROUP BY language ORDER BY count(*) desc LIMIT 3;
 INSERT INTO clients ( username, first_name, last_name, email, address)
-Values ("epsu", "Evert", "Kärp", "evert.karp@ametikool.ee", "Tehnika 1-64");
+Values ("Patu", "Karl-Patric", "Rohi", "Karl-Patric.rohi@ametikool.ee", "Nõmpa küla, männiku talu");
 UPDATE books SET language = "Eesti" WHERE id = 1;
 DELETE FROM orders WHERE id = 2300;
 INSERT INTO clients ( username, first_name, last_name, email, address)
 values
-("jams", "Mark", "Neer", "markk@gmail.com", "Noorsoo"),
-("ramos", "Valdis", "Joss", "valdisj@gmail.com", "paldisk"),
-("juur", "Raido", "Libe", "liberaido@gmail.com", "pikk"),
-("naos", "Toomas", "Tuurik", "tuurikt@gmail.com", "kevade"),
-("pensora", "Roomet", "Rool", "roometr@gmail.com", "smuuli");
+("Ants", "Anti", "Nuga", "AntiN@gmail.com", "Vändi 2"),
+("Kari", "Mari", "Kala", "KalaMari@gmail.com", "Vändi 3"),
+("Lipsu", "Lible", "Toom", "LibleToom@gmail.com", "Vändi 4"),
+("Tom", "Tommy", "Koomas", "KoomasTom@gmail.com", "Vändi 5"),
+("And", "Andis", "Nuga", "NugaAnd@gmail.com", "Vändi 6");
 INSERT INTO orders (delivery_address, order_date, status, client_id, book_id)
 VALUES
-("5423 Juhan Smuuli", "2017-9-11", "sent",
+("4200 Vändise Vändra", "2002-2-5", "sent",
 (SELECT id FROM d110072_raamatud.clients WHERE username = "mcage1o"),
 (SELECT id FROM d110072_raamatud.books WHERE title = "Vendetta"));
 UPDATE d110072_raamatud.books
