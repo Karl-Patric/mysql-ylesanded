c421295838977a5cd0c2da30955b7700d2be308b
Karl-Patric <92147164+Karl-Patric@users.noreply.github.com>
2022-10-11 14:40:43 +0300

    Update Kolmas mysql.sql

--- Kolmas mysql.sql
+++ Kolmas mysql.sql
@@ -1,23 +1,21 @@
-SELECT * FROM d110079sd465665.orders;
-INSERT INTO clients (username, first_name, last_name, email, address)
-VALUES ("patr1c", "Karl-Patric", "Rohi", "Karl-Patric.rohi@ametikool.ee", "Nõmpa küla Männiku talu");
-UPDATE books
-SET language = "Eesti"
-WHERE id = 1;
-DELETE FROM orders
-WHERE id = 2300;
-INSERT INTO clients (username, first_name, last_name, email, address)
-VALUES 
-("Pets", "Peeter", "Silo", "PetsSilo@gmail.com", "Silomaa"),
-("Ants", "Anti", "Nuga", "AntiNuga@gmail.com", "Antsumaa"),
-("Kalts", "Mari", "Kala", "KalaMari@gmail.com", "Kalamaa"),
-("Susi", "Marko", "Suslik", "Suslik@gmail.com", "Susimaa"),
-("Anjusha", "Anna", "Minna", "keriis@gmail.com", "Andisminna");
+SELECT * FROM d110072_raamatud.books WHERE release_date % 2 = 0;
+SELECT count(*), language FROM d110072_raamatud.books GROUP BY language ORDER BY count(*) desc LIMIT 3;
+INSERT INTO clients ( username, first_name, last_name, email, address)
+Values ("epsu", "Evert", "Kärp", "evert.karp@ametikool.ee", "Tehnika 1-64");
+UPDATE books SET language = "Eesti" WHERE id = 1;
+DELETE FROM orders WHERE id = 2300;
+INSERT INTO clients ( username, first_name, last_name, email, address)
+values
+("jams", "Mark", "Neer", "markk@gmail.com", "Noorsoo"),
+("ramos", "Valdis", "Joss", "valdisj@gmail.com", "paldisk"),
+("juur", "Raido", "Libe", "liberaido@gmail.com", "pikk"),
+("naos", "Toomas", "Tuurik", "tuurikt@gmail.com", "kevade"),
+("pensora", "Roomet", "Rool", "roometr@gmail.com", "smuuli");
 INSERT INTO orders (delivery_address, order_date, status, client_id, book_id)
 VALUES
-("2323 Chinook Plaza", "2018-12-13", "sent",
-(SELECT id FROM d107230_leopard.clients WHERE username = "mcage1o"),
-(SELECT id FROM d107230_leopard.books WHERE title = "Vendetta"));
-UPDATE d107230_leopard.books
+("5423 Juhan Smuuli", "2017-9-11", "sent",
+(SELECT id FROM d110072_raamatud.clients WHERE username = "mcage1o"),
+(SELECT id FROM d110072_raamatud.books WHERE title = "Vendetta"));
+UPDATE d110072_raamatud.books
 set price = price * 1.05, pages = pages - 5  where id > 0;
 DELETE FROM authors WHERE id IN (SELECT authors.id FROM authors LEFT JOIN book_authors ON authors.id = book_authors.author_id WHERE book_authors.id is NULL);
