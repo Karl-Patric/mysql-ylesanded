0c66489452937978219f48fa51f87ab0ce6f6255
Karl-Patric <92147164+Karl-Patric@users.noreply.github.com>
2022-10-11 14:36:58 +0300

    Update esimene_sql.sql

--- esimene_sql.sql
+++ esimene_sql.sql
@@ -1,9 +1,6 @@
-SELECT * FROM d110079sd465665.orders;
-SELECT * FROM books WHERE release_date >= 2010 AND type = 'new';
-SELECT title, release_date, price, type FROM books WHERE release_date < 1970 AND type= 'used' AND price < 20 ;
-SELECT count(*) AS "Tellimuste arv" , YEAR (order_date) AS Aasta FROM orders GROUP BY YEAR(order_date);
-SELECT ROUND(SUM(price),2) AS "Ümardatud" FROM orders LEFT JOIN books ON orders.book_id = books.id GROUP BY YEAR(order_date);
-SELECT SUM(price), COUNT(*) FROM orders LEFT JOIN books ON orders.book_id = books.id WHERE YEAR (order_date) = 2017;
-SELECT SUM(price), COUNT(*), username FROM clients LEFT JOIN orders ON orders.client_id = clients.id LEFT JOIN books ON orders.book_id= books.id WHERE YEAR (order_date) = 2017 GROUP BY client_id;
-SELECT COUNT(*) FROM orders LEFT JOIN books ON orders.book_id = books.id WHERE YEAR (order_date) = 2017 GROUP BY books.id ORDER BY count(*) desC LIMIT 10;
-SELECT * FROM books WHERE price > ALL(SELECT avg(price) FROM books);
+SELECT * FROM books WHERE release_date >=2010 AND type = "new" ORDER BY title;
+SELECT * FROM books WHERE release_date <=1970 AND type = "used" AND price <=20 ORDER BY title, price, type;
+SELECT count(*) as "Tellimuste arv" , year(order_date) as "aasta" FROM orders where order_date group by year(order_date);
+SELECT ROUND(SUM(price),2) as "ÜMARDATUD" FROM orders LEFT JOIN books ON orders.book_id = books.id GROUP BY YEAR(order_date);
+SELECT count(*), SUM(books.price) FROM orders LEFT JOIN books ON orders.book_id = books.id WHERE YEAR(order_date) = 2017;
+SELECT  count(*), SUM(books.price), clients.username FROM clients LEFT JOIN orders ON orders.client_id = clients.id LEFT JOIN books ON orders.book_id = books.id WHERE year(orders.order_date) = 2017 GROUP BY clients.id ORDER BY SUM(books.price) DESC;
